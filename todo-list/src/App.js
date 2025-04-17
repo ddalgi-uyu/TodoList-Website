1db4fe67bcfd88c8d61b59f433bc7028e0cd8467
Nelly <kchen0116@gmail.com>
2025-04-17 15:29:56 +0800

    Add basic react router

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,5 +1,28 @@
 import './App.css';
 import todoList from './components/todoList';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "login",
+    element: <div>Login page</div>,
+  },
+]);
 
 function App() {
   return (
