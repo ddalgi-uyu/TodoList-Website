acff64728e538617a8b063635b7eb702b297a1ce
Nelly <kchen0116@gmail.com>
2025-04-18 20:14:27 +0800

    Add functionality to show todo list items

--- todo-list/src/components/todoList.jsx
+++ todo-list/src/components/todoList.jsx
@@ -1,8 +1,28 @@
-const todoList = () => {
+import { useState } from 'react'
+import TodoForm from './TodoForm'
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+    const [todoList, setTodoList] = useState([{id: 1, text: "sample todoList", done: false}]);
+
+    const atAddItem = () => {
+
+    }
+
+    const atToggleItem = () => {
+
+    }
 
     return (
-        <todoItem/>
+        <section>
+            <TodoForm/>
+            <ul>
+                {todoList.map((item) => (
+                <TodoItem key={item.id} id={item.id} done={item.done} text={item.text} onToggleItem={atToggleItem} />
+                ))}
+            </ul>
+        </section>
     )
 }
 
-export default todoList
+export default TodoList
